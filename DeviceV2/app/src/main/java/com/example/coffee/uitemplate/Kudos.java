65b2491414c96cfaf7714553a20d71e90266fe9e
koliman <kyle.oliman@gmail.com>
2014-12-08 10:37:28 -0800

    Kudos stuff
    Kudos stuff placed in Device V2 according to where comment said to
    implement

--- DeviceV2/app/src/main/java/com/example/coffee/uitemplate/Kudos.java
+++ DeviceV2/app/src/main/java/com/example/coffee/uitemplate/Kudos.java
@@ -8,6 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 
 public class Kudos extends Activity {
@@ -22,6 +31,70 @@ public class Kudos extends Activity {
             @Override
             public void onClick(View v) {
                 // insert code to implement Kudos here
+                Socket socket = new Socket();
+                OutputStreamWriter osw;
+                String sendKudos = "Sending Kudos";
+
+                OutputStream outputStream = null;
+                InputStream inputStream = null;
+                try {
+                    /**
+                     * Create a client socket with the host,
+                     * port, and timeout information
+                     */
+                    socket.bind(null);
+                    socket.connect((new InetSocketAddress(host, 4545)), 500);
+                    //Note: "host" -- no idea how to access IP address, should have asked before
+                    //May also need to move this to an AsyncTask outside of onCreate
+                    //Got most of this from the WifiP2PDemo
+
+                    outputStream = socket.getOutputStream();
+                    inputStream = socket.getInputStream();
+                    osw = new OutputStreamWriter(outputStream, "UTF-8");
+
+                    //Send string of "Sending Kudos", fulfilling the "Kudos" needed to read a Kudos being sent
+                    //May need to use DataInputStream readUTF() method on the receiving side
+                    osw.write(sendKudos, 0, sendKudos.length());
+                    osw.flush();
+                    //outputStream.close();
+                    //inputStream.close();
+                } catch (IOException e) {
+                    //catch logic
+                    //e.printStackTrace();
+                }
+                /**
+                 * Clean up any open sockets or streams when done
+                 * transferring or if an exception occurred.
+                 */
+                finally {
+                    if (socket != null) {
+                        if (socket.isConnected()) {
+                            try {
+                                socket.close();
+                            } catch (IOException e) {
+                                //e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    if (inputStream != null) {
+                        try {
+                            inputStream.close();
+                        } catch (IOException e) {
+                            //e.printStackTrace();
+                        }
+                    }
+
+                    if (outputStream != null) {
+                        try {
+                            outputStream.close();
+                        } catch (IOException e) {
+                            //e.printStackTrace();
+                        }
+                    }
+                }
+
+                Toast.makeText(Kudos.this, "Kudos Sent!", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -48,4 +121,5 @@ public class Kudos extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
